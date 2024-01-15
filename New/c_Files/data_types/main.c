7868792abd2af524426e9c46eb1d9de5ba04a216
jigar.maheshwari <jigar.maheshwari@volansys.com>
2024-01-15 16:01:31 +0530

    data type work

---
+++ New/c_Files/data_types/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<limits.h>
+
+void main(){
+    printf("min char = %d \n",CHAR_MIN);
+    printf("max char = %d \n",CHAR_MAX);
+    printf("size of char = %ld \n",sizeof(char));
+
+    printf("signed char min = %d\n",SCHAR_MIN);
+}
