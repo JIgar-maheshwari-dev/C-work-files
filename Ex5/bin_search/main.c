0ceee8a92ab035709f601ed9122e6b72731eaba9
jigar.maheshwari <jigar.maheshwari@volansys.com>
2024-01-22 17:00:12 +0530

    changes

--- Ex5/bin_search/main.c
+++ Ex5/bin_search/main.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
+#include"hed.h"
 
 int main() {
 	int a[]={1,4,9,15,23,45,66,79,85,101};
 	int p=23;
 	int i=bin_search(a,p);
-	
+	int j;
+	for( j=1; a[j]!='\0' ;j++){
+                printf("%d \n",a[j]);
+        }
+	printf( "length is = %d\n",j  );
 	printf(" %d is found in array at index %d\n",p,i);
 	printf("-1 means not founf \n\n");
 
-
+                                
 	return 0;
 }
 
+isfsfsdfsd
